346833a1fd0cd3c46a30463716ddb9520486246b
Denis Romazin <romazin777@gmail.com>
2022-09-13 22:15:35 +0500

    add project Sum

---
+++ Example_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите 2 целых числа :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1 + num2;
+Console.WriteLine("Сумма введенных чисел : " + sum);
